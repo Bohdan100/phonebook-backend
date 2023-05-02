b866761ba5595bfdb71d3543460c3105ac2794a3
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-05-03 00:11:28 +0300

    err

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = require("./app");
 const { PORT } = process.env;
 
 db.authenticate()
+  .then(console.log("db", db))
   .then(
     app.listen(PORT, () => {
       console.log("Database connection successful");
       console.log("PORT", PORT);
-      console.log("db", db);
     })
   )
   .catch((err) => {
-    console.log(err.message);
+    console.log(err);
+    console.log("err.message", err.message);
     process.exit(1);
   });
